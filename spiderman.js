ffe8ebeb050f047e971efc904feac8a48172feaf
chazzfit <basson_j@esme.fr>
2017-03-21 19:21:26 +0100

    spidey ajouté

--- spiderman.js
+++ spiderman.js
@@ -1,25 +1,33 @@
+const {Poney}=require('./poney');
+
+
 class Spiderman {
 
-    constructor() {
-        this.poney = 1;
-        this.energie = 10;
-        // this.energieponey = 10;
-        // this.maxEnergieDeadpool = 10;
+  constructor() {
+    this.poney = 1;
+    this.energie = 10;
+    // this.energieponey = 10;
+    // this.maxEnergieDeadpool = 10;
 
 
+  }
 
-    }
+  rodeo(tab) {
+    return new Promise((resolve, reject) => {
 
-    rodeo() {
 
-      setTimeout(() =>
-        //poney.qcch
-        , Math.floor((Math.random() * 3000) + 1000));
-        //renvoie une valeur en 1000 et 3000
+      let rdRodeo = Math.floor((Math.random() * 3000) + 1000);
+      if (rdRodeo > 2000) {
+        setTimeout(() => {
+          tab.isAvailable = false;
 
-    }
+
+        }, 1000);
+      }
+
+
+    });
+  }
 }
-const spider=new Spiderman();
-spider.rodeo();
 
-module.exports={Spiderman};
+module.exports = {Spiderman};
